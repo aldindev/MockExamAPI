e44a37df4cebaf3f6979ea9e66945b037d49301f
aldinamaya <125940758+aldinamaya@users.noreply.github.com>
2023-12-18 10:42:02 +0800

    First commit"

---
+++ QuizAPI/Migrations/20231122061943_nvarchar change to 150.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizAPI.Migrations
+{
+    public partial class nvarcharchangeto150 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "QnInWords",
+                table: "Questions",
+                type: "nvarchar(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option4",
+                table: "Questions",
+                type: "nvarchar(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option3",
+                table: "Questions",
+                type: "nvarchar(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option2",
+                table: "Questions",
+                type: "nvarchar(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option1",
+                table: "Questions",
+                type: "nvarchar(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageName",
+                table: "Questions",
+                type: "nvarchar(150)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "QnInWords",
+                table: "Questions",
+                type: "nvarchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option4",
+                table: "Questions",
+                type: "nvarchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option3",
+                table: "Questions",
+                type: "nvarchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option2",
+                table: "Questions",
+                type: "nvarchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option1",
+                table: "Questions",
+                type: "nvarchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageName",
+                table: "Questions",
+                type: "nvarchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldNullable: true);
+        }
+    }
+}
